3fc33cfa27359d3b51d765d2ac8f2efc243bad25
minhl337 <imnotminh@gmail.com>
2019-06-26 10:00:24 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("sup")
+})
+
+const port = process.env.PORT || 5000
+
+app.listen(port,()=>console.log(`App is listening on ${port}`));
