027a04ccaa76c6be1f906be965cbb4b84c215702
minhl337 <imnotminh@gmail.com>
2019-07-08 23:55:57 -0500

    profile done

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,6 +6,9 @@ import {getProfileById} from '../../actions/profile'
 import {Link} from 'react-router-dom'
 import ProfileTop from './ProfileTop'
 import ProfileAbout from './ProfileAbout'
+import ProfileExperience from './ProfileExperience'
+import ProfileEducation from './ProfileEducation'
+import ProfileGithub from './ProfileGithub'
 
 const Profile = ({match,getProfileById,profile:{profile,loading},auth}) => {
 
@@ -24,6 +27,28 @@ const Profile = ({match,getProfileById,profile:{profile,loading},auth}) => {
                 <div className="profile-grid my-1">
                     <ProfileTop profile={profile}/>
                     <ProfileAbout profile={profile}/>
+                    <div className='profile-exp bg-white p-2' >
+                        <h2 className='text-primary'>Experience</h2>
+                        {profile.experience.length > 0 ? (<Fragment>
+                            {profile.experience.map(experience=>(
+                                <ProfileExperience key={experience._id} experience={experience}/>
+                            ))}
+                        </Fragment>) : (<h4>No experiences</h4>)}
+                    </div>
+
+                    <div className='profile-edu bg-white p-2'>
+                        <h2 className='text-primary'>Education</h2>
+                        {profile.education.length > 0 ? (<Fragment>
+                            {profile.education.map(education=>(
+                                <ProfileEducation key={education._id} education={education}/>
+                            ))}
+                        </Fragment>) : (<h4>No education</h4>)}
+                    </div>
+
+                    {/* {profile.githubusername && (
+                        <ProfileGithub username={profile.githubusername}/>
+                    )} */}
+
                 </div>
             </Fragment>}
         </div>
